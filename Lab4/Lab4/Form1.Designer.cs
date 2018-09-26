ff164dfbe1efac71ef58a83fc6d8a74f556193f3
AntonyMo <lexmqwerty@gmail.com>
2018-09-26 13:07:12 +0300

    Lab 4 implemented

---
+++ Lab4/Lab4/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLoadFile = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelReadTime = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxSearchWord = new System.Windows.Forms.TextBox();
+            this.buttonStrictSearch = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelStrictSearchTime = new System.Windows.Forms.Label();
+            this.listBoxSearchResult = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // buttonLoadFile
+            // 
+            this.buttonLoadFile.Location = new System.Drawing.Point(13, 13);
+            this.buttonLoadFile.Name = "buttonLoadFile";
+            this.buttonLoadFile.Size = new System.Drawing.Size(131, 23);
+            this.buttonLoadFile.TabIndex = 0;
+            this.buttonLoadFile.Text = "Чтение из файла";
+            this.buttonLoadFile.UseVisualStyleBackColor = true;
+            this.buttonLoadFile.Click += new System.EventHandler(this.buttonLoadFile_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(165, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Время чтения из файла:";
+            // 
+            // labelReadTime
+            // 
+            this.labelReadTime.AutoSize = true;
+            this.labelReadTime.Location = new System.Drawing.Point(311, 18);
+            this.labelReadTime.Name = "labelReadTime";
+            this.labelReadTime.Size = new System.Drawing.Size(0, 13);
+            this.labelReadTime.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Слово для поиска:";
+            // 
+            // textBoxSearchWord
+            // 
+            this.textBoxSearchWord.Location = new System.Drawing.Point(161, 67);
+            this.textBoxSearchWord.Name = "textBoxSearchWord";
+            this.textBoxSearchWord.Size = new System.Drawing.Size(295, 20);
+            this.textBoxSearchWord.TabIndex = 4;
+            // 
+            // buttonStrictSearch
+            // 
+            this.buttonStrictSearch.Location = new System.Drawing.Point(13, 113);
+            this.buttonStrictSearch.Name = "buttonStrictSearch";
+            this.buttonStrictSearch.Size = new System.Drawing.Size(131, 23);
+            this.buttonStrictSearch.TabIndex = 5;
+            this.buttonStrictSearch.Text = "Четкий поиск";
+            this.buttonStrictSearch.UseVisualStyleBackColor = true;
+            this.buttonStrictSearch.Click += new System.EventHandler(this.buttonStrictSearch_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(165, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Время поиска слова:";
+            // 
+            // labelStrictSearchTime
+            // 
+            this.labelStrictSearchTime.AutoSize = true;
+            this.labelStrictSearchTime.Location = new System.Drawing.Point(286, 118);
+            this.labelStrictSearchTime.Name = "labelStrictSearchTime";
+            this.labelStrictSearchTime.Size = new System.Drawing.Size(0, 13);
+            this.labelStrictSearchTime.TabIndex = 7;
+            // 
+            // listBoxSearchResult
+            // 
+            this.listBoxSearchResult.FormattingEnabled = true;
+            this.listBoxSearchResult.Location = new System.Drawing.Point(15, 153);
+            this.listBoxSearchResult.Name = "listBoxSearchResult";
+            this.listBoxSearchResult.Size = new System.Drawing.Size(441, 147);
+            this.listBoxSearchResult.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 322);
+            this.Controls.Add(this.listBoxSearchResult);
+            this.Controls.Add(this.labelStrictSearchTime);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonStrictSearch);
+            this.Controls.Add(this.textBoxSearchWord);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelReadTime);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonLoadFile);
+            this.MinimumSize = new System.Drawing.Size(500, 360);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLoadFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelReadTime;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxSearchWord;
+        private System.Windows.Forms.Button buttonStrictSearch;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelStrictSearchTime;
+        private System.Windows.Forms.ListBox listBoxSearchResult;
+    }
+}
+
---
+++ Lab4/Lab4/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace Lab4
+{
+    public partial class Form1 : Form
+    {
+        List<string> list = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonLoadFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Filter = "текстовые файлы|*.txt";
+
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                //хмммммммм
+                list.Clear();
+
+                Stopwatch t = new Stopwatch();
+                t.Start();
+                
+                //У меня на Windows очень странно читаются файлы. Нормально работает только с юникодом
+                string text = File.ReadAllText(fd.FileName, Encoding.Unicode);
+
+                char[] separators = new char[] { ' ', '.', ',', '!', '/', '\t', '\n' };
+
+                string[] textArray = text.Split(separators);
+                foreach (string strTemp in textArray)
+                {
+                    string str = strTemp.Trim();
+
+                    if (!list.Contains(str))
+                        list.Add(str);
+                }
+
+                t.Stop();
+                this.labelReadTime.Text = t.Elapsed.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Необходимо выбрать файл");
+            }
+        }
+
+        private void buttonStrictSearch_Click(object sender, EventArgs e)
+        {
+            string word = this.textBoxSearchWord.Text.Trim();
+
+            if (!string.IsNullOrWhiteSpace(word) && list.Count > 0)
+            {
+                string wordUpper = word.ToUpper();
+
+                List<string> tempList = new List<string>();
+
+                Stopwatch t = new Stopwatch();
+                t.Start();
+
+                foreach (string str in list)
+                {
+                    if(str.ToUpper().Contains(wordUpper))
+                    {
+                        tempList.Add(str);
+                    }
+                }
+
+                t.Stop();
+                this.labelStrictSearchTime.Text = t.Elapsed.ToString();
+
+                this.listBoxSearchResult.BeginUpdate();
+
+                this.listBoxSearchResult.Items.Clear();
+
+                foreach (string str in tempList)
+                {
+                    this.listBoxSearchResult.Items.Add(str);
+                }
+
+                this.listBoxSearchResult.EndUpdate();
+            }
+            else
+            {
+                MessageBox.Show("Необходимо выбрать файл или ввести слово для поиска");
+            }
+        }
+    }
+}
